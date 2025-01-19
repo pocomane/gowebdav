f6907e783c22bf137cb96d91ea4037fa228123a7
pocomane <none@mail.com>
2025-01-19 10:36:54 +0100

    Release name and version are taken from the source code

--- make.sh
+++ make.sh
@@ -13,11 +13,11 @@ build(){
     EXT=".exe"
   fi
   go mod tidy
-  go build -ldflags "-s -w" -o "$OUTDIR/gowebdav-$VERSION-$GOARCH-${OSTAG}${EXT}"
+  go build -ldflags "-s -w" -o "$OUTDIR/$APP_TAG-$GOARCH-${OSTAG}${EXT}"
 }
 
 export OUTDIR="./build/release/"
-export VERSION="v0.1"
+export APP_TAG=$(cat main.go | sed -n 's/.*APP_TAG.*=.*"\([^"]*\)"/\1/p' | tail -n 1)
 
 GOARCH="amd64" GOOS="linux" build
 GOARCH="amd64" GOOS="windows" build
