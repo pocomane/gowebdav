684d7369af54d79ff6164b00ab346ea10d7eaaf4
pocomane <none@mail.com>
2025-01-19 17:52:44 +0100

    Using spaces instead of tabs

--- main.go
+++ main.go
@@ -4,39 +4,39 @@ package main
 // THIS SOFTWARE IS PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"strings"
+  "context"
+  "errors"
+  "fmt"
+  "strings"
   "strconv"
   "path"
-	"net"
-	"net/http"
-	"net/http/httputil"
-	"golang.org/x/net/webdav"
-	"io/fs"
+  "net"
+  "net/http"
+  "net/http/httputil"
+  "golang.org/x/net/webdav"
+  "io/fs"
   "os"
-	"time"
+  "time"
   "runtime"
 )
 
 const (
   APP_TAG = "GoWebDAV-v0.1"
 
-	ERROR   = 0
-	WARNING = 1
-	INFO    = 2
+  ERROR   = 0
+  WARNING = 1
+  INFO    = 2
   DEBUG   = 3
 
-	GDW_ENV_PREFIX  = "GWD_"
-	ENV_VERBOSITY   = GDW_ENV_PREFIX + "VERBOSE"
-	ENV_HOST        = GDW_ENV_PREFIX + "HOST"
-	ENV_PORT        = GDW_ENV_PREFIX + "PORT"
-	ENV_PATH        = GDW_ENV_PREFIX + "PATH"
-	ENV_PREFIX      = GDW_ENV_PREFIX + "PREFIX"
-	ENV_SERVE_MODE  = GDW_ENV_PREFIX + "SERVE_MODE"
-	ENV_ZONE_HEADER = GDW_ENV_PREFIX + "ZONE_HEADER"
-	ENV_CLEAN_DEST  = GDW_ENV_PREFIX + "CLEAN_DESTINATION"
+  GDW_ENV_PREFIX  = "GWD_"
+  ENV_VERBOSITY   = GDW_ENV_PREFIX + "VERBOSE"
+  ENV_HOST        = GDW_ENV_PREFIX + "HOST"
+  ENV_PORT        = GDW_ENV_PREFIX + "PORT"
+  ENV_PATH        = GDW_ENV_PREFIX + "PATH"
+  ENV_PREFIX      = GDW_ENV_PREFIX + "PREFIX"
+  ENV_SERVE_MODE  = GDW_ENV_PREFIX + "SERVE_MODE"
+  ENV_ZONE_HEADER = GDW_ENV_PREFIX + "ZONE_HEADER"
+  ENV_CLEAN_DEST  = GDW_ENV_PREFIX + "CLEAN_DESTINATION"
 
   MODE_FOLDER = "folder"
   MODE_ZONE   = "zone"
@@ -44,10 +44,10 @@ const (
 )
 
 type app struct {
-	root_handler webdav.Handler
-	zone_handler map[string]*webdav.Handler
-	host         string
-	port         uint16
+  root_handler webdav.Handler
+  zone_handler map[string]*webdav.Handler
+  host         string
+  port         uint16
   verbosity    uint16
   hide_root    bool
   zone_header  string
@@ -56,103 +56,103 @@ type app struct {
 }
 
 type logOpt struct {
-	zero  bool
-	skip  uint16
-	level uint16
+  zero  bool
+  skip  uint16
+  level uint16
 }
 
 func (t*app) shouldLog(opt logOpt) bool { // TODO : remove ???
-	if opt.level > t.verbosity {
-		return false
-	}
+  if opt.level > t.verbosity {
+    return false
+  }
   return true
 }
 
 func (t*app) log(msgs ...interface{}) {
-	opt := logOpt{}
-	for _, m := range msgs {
-		switch a := m.(type) {
-		case logOpt:
-			if a.skip != 0 {
-				opt.skip = a.skip
-			}
-			if a.level != 0 {
-				opt.level = a.level
-			}
-			if a.zero {
-				opt = logOpt{}
-			}
-		}
-	}
-	if !t.shouldLog(opt) {
-		return
-	}
-	sourcepos := "?:?"
-	if _, b, c, d := runtime.Caller(1 + int(opt.skip)); d {
+  opt := logOpt{}
+  for _, m := range msgs {
+    switch a := m.(type) {
+    case logOpt:
+      if a.skip != 0 {
+        opt.skip = a.skip
+      }
+      if a.level != 0 {
+        opt.level = a.level
+      }
+      if a.zero {
+        opt = logOpt{}
+      }
+    }
+  }
+  if !t.shouldLog(opt) {
+    return
+  }
+  sourcepos := "?:?"
+  if _, b, c, d := runtime.Caller(1 + int(opt.skip)); d {
     i := strings.LastIndex(b, "/")
     if i > 0 {
       b = b[i+1:]
     }
-		sourcepos = fmt.Sprintf("%v:%v", b, c)
-	}
-	severity := "DEBUG"
+    sourcepos = fmt.Sprintf("%v:%v", b, c)
+  }
+  severity := "DEBUG"
   switch opt.level {
     case ERROR: severity = "ERROR"
     case WARNING: severity = "WARNING"
     case DEBUG: severity = "DEBUG"
   }
-	fmt.Printf("%s [%s %v]", severity, time.Now().Format(time.RFC3339), sourcepos)
-	for _, v := range msgs {
-		switch v.(type) {
-		default:
-			fmt.Printf(" %#v", v)
+  fmt.Printf("%s [%s %v]", severity, time.Now().Format(time.RFC3339), sourcepos)
+  for _, v := range msgs {
+    switch v.(type) {
+    default:
+      fmt.Printf(" %#v", v)
     case error:
-			fmt.Printf(" %v", v)
+      fmt.Printf(" %v", v)
     case string:
-			fmt.Printf(" %v", v)
-		case logOpt:
-		}
-	}
-	fmt.Printf("\n")
+      fmt.Printf(" %v", v)
+    case logOpt:
+    }
+  }
+  fmt.Printf("\n")
 }
 
 func (t*app) logRequest(req *http.Request, err error) {
-	sk := logOpt{skip: 1}
-	if err == nil || errors.Is(err, fs.ErrNotExist) {
-		return
-	}
-	requestDump, _ := httputil.DumpRequest(req, true)
-	rd := string(requestDump)
-	if errors.Is(err, webdav.ErrConfirmationFailed) ||
-		errors.Is(err, webdav.ErrForbidden) ||
-		errors.Is(err, webdav.ErrLocked) ||
-		errors.Is(err, webdav.ErrNoSuchLock) {
-		t.log(sk, logOpt{level: INFO}, "WebDAV request error", "error:", err, "request:", rd)
-		return
-	} else {
-		t.log(sk, logOpt{level: ERROR}, "internal handler error", "error:", err, "request:", rd)
-	}
+  sk := logOpt{skip: 1}
+  if err == nil || errors.Is(err, fs.ErrNotExist) {
+    return
+  }
+  requestDump, _ := httputil.DumpRequest(req, true)
+  rd := string(requestDump)
+  if errors.Is(err, webdav.ErrConfirmationFailed) ||
+    errors.Is(err, webdav.ErrForbidden) ||
+    errors.Is(err, webdav.ErrLocked) ||
+    errors.Is(err, webdav.ErrNoSuchLock) {
+    t.log(sk, logOpt{level: INFO}, "WebDAV request error", "error:", err, "request:", rd)
+    return
+  } else {
+    t.log(sk, logOpt{level: ERROR}, "internal handler error", "error:", err, "request:", rd)
+  }
 }
 
 func (t*app) Bind() (net.Listener, error) {
-	t.root_handler.Logger = func(req *http.Request, err error) { t.logRequest(req, err) }
+  t.root_handler.Logger = func(req *http.Request, err error) { t.logRequest(req, err) }
 
-	addr := fmt.Sprintf("%s:%d", t.host, t.port)
+  addr := fmt.Sprintf("%s:%d", t.host, t.port)
 
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
+  ln, err := net.Listen("tcp", addr)
+  if err != nil {
     t.log(logOpt{level:ERROR}, err)
-		return nil, err
-	}
+    return nil, err
+  }
 
-	return ln, nil
+  return ln, nil
 }
 
 func (t*app) SelectZoneHandler(r *http.Request) *webdav.Handler{
   zone := r.Header.Get(t.zone_header)
   var handler *webdav.Handler
   if t.hide_root || t.zone_handler != nil {
-	  t.log(logOpt{level: DEBUG}, "using zone", "'" + zone + "'")
+    t.log(logOpt{level: DEBUG}, "using zone", "'" + zone + "'")
   }
   if !t.hide_root && zone == "" {
     handler = &t.root_handler
@@ -176,9 +176,9 @@ func (t*app) WebDAVHandler(w http.ResponseWriter, r *http.Request) {
 
   handler := t.SelectZoneHandler(r)
   if handler == nil {
-		t.log(logOpt{level: DEBUG}, "returning 404 Not Found for", r.URL.Path)
+    t.log(logOpt{level: DEBUG}, "returning 404 Not Found for", r.URL.Path)
     w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "404 Not Found")
+    fmt.Fprintf(w, "404 Not Found")
     return
   }
 
@@ -202,48 +202,48 @@ func (t*app) WebDAVHandler(w http.ResponseWriter, r *http.Request) {
     }
 
   case http.MethodHead:
-  	w = nothingWriter{w}
+    w = nothingWriter{w}
     fallthrough
   // RFC4918 allows to GET over a collection to return anything the implementation
   // found useful. A common choice is to let it behave like PROPFIND. Also HEAD
   // on collaction behaves in the same way of PROPFIND.
   case http.MethodGet:
-  	file_stat, err := handler.FileSystem.Stat(context.TODO(), r.URL.Path)
-  	if err == nil && file_stat.IsDir() {
-  		r.Method = "PROPFIND"
-  		if r.Header.Get("Depth") == "" {
-  			r.Header.Add("Depth", "1")
-  		}
-  	}
+    file_stat, err := handler.FileSystem.Stat(context.TODO(), r.URL.Path)
+    if err == nil && file_stat.IsDir() {
+      r.Method = "PROPFIND"
+      if r.Header.Get("Depth") == "" {
+        r.Header.Add("Depth", "1")
+      }
+    }
   }
 
-	handler.ServeHTTP(w, r)
+  handler.ServeHTTP(w, r)
 }
 
 func (t*app) Run(ln net.Listener) error {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     t.WebDAVHandler(w, r)
-	})
-	server := &http.Server{}
+  })
+  server := &http.Server{}
 
-	err := server.Serve(ln)
-	if err != nil {
-		t.log(logOpt{level:ERROR}, err)
-	} else {
-		t.log(logOpt{level:INFO}, "done")
-	}
-	return err
+  err := server.Serve(ln)
+  if err != nil {
+    t.log(logOpt{level:ERROR}, err)
+  } else {
+    t.log(logOpt{level:INFO}, "done")
+  }
+  return err
 }
 
 // Main and Configuration
 
 func getenv(name, def string) string {
-	val := os.Getenv(name)
-	if val == "" {
-		return def
-	}
-	return val
+  val := os.Getenv(name)
+  if val == "" {
+    return def
+  }
+  return val
 }
 
 type subLockSystem struct{
@@ -300,7 +300,7 @@ func (t*app) ZoneConfig(serve_path string) {
         name := item.Name()
         wh := t.root_handler // clone
         wh.LockSystem = &subLockSystem{t.root_handler.LockSystem, name}
-	      wh.FileSystem = webdav.Dir(path.Join(serve_path, name))
+        wh.FileSystem = webdav.Dir(path.Join(serve_path, name))
         t.zone_handler[name] = &wh
       }
   }
@@ -310,34 +310,34 @@ func (t*app) ParseConfig() {
 
   t.root_handler.LockSystem = &subLockSystem{webdav.NewMemLS(), ""}
 
-	verb, err := strconv.Atoi(getenv(ENV_VERBOSITY, "2"))
-	if err != nil {
-		t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_VERBOSITY, err)
-		return
-	}
+  verb, err := strconv.Atoi(getenv(ENV_VERBOSITY, "2"))
+  if err != nil {
+    t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_VERBOSITY, err)
+    return
+  }
   t.verbosity = uint16(verb)
 
-	t.host = getenv(ENV_HOST, "127.0.0.1")
+  t.host = getenv(ENV_HOST, "127.0.0.1")
 
-	serve_path := getenv(ENV_PATH, "./")
-	t.root_handler.FileSystem = webdav.Dir(path.Clean(serve_path))
+  serve_path := getenv(ENV_PATH, "./")
+  t.root_handler.FileSystem = webdav.Dir(path.Clean(serve_path))
 
-	t.root_handler.Prefix = getenv(ENV_PREFIX, "")
+  t.root_handler.Prefix = getenv(ENV_PREFIX, "")
 
-	port, err := strconv.Atoi(getenv(ENV_PORT, "0"))
-	if err != nil {
-		t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_PORT, err)
-		return
-	}
-	t.port = uint16(port)
+  port, err := strconv.Atoi(getenv(ENV_PORT, "0"))
+  if err != nil {
+    t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_PORT, err)
+    return
+  }
+  t.port = uint16(port)
 
-	t.zone_header = getenv(ENV_ZONE_HEADER, "Authorization")
+  t.zone_header = getenv(ENV_ZONE_HEADER, "Authorization")
 
-	serve_mode := getenv(ENV_SERVE_MODE, MODE_FOLDER)
+  serve_mode := getenv(ENV_SERVE_MODE, MODE_FOLDER)
   switch serve_mode {
   default:
-		t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_SERVE_MODE)
-		return
+    t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_SERVE_MODE)
+    return
   case MODE_FOLDER: // nothing else to do
   case MODE_ZONE:
     t.ZoneConfig(serve_path)
@@ -348,19 +348,19 @@ func (t*app) ParseConfig() {
 
   clean_dest := strings.ToLower(getenv(ENV_CLEAN_DEST, "no"))
   if clean_dest != "no" && clean_dest != "yes" {
-		t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_CLEAN_DEST)
-		return
+    t.log(logOpt{level: ERROR}, "Wrong value for variable "+ENV_CLEAN_DEST)
+    return
   }
   t.clean_dest = (clean_dest == "yes")
 
-	fmt.Printf("Serving mode: [%s]\n", serve_mode)
-	fmt.Printf("Verbosity level: [%d]\n", t.verbosity)
-	fmt.Printf("Serving content of: [%s]\n", serve_path)
-	fmt.Printf("Removing prefix from requested URL: [%s]\n", t.root_handler.Prefix)
-	fmt.Printf("Serving URL with prefix: [%s]\n", t.root_handler.Prefix)
-	fmt.Printf("Serving content at host: [%s]\n", t.host)
-	fmt.Printf("Trying configured port: [%d]\n", port)
-	fmt.Printf("Clean destination in copy request: [%s]\n", clean_dest)
+  fmt.Printf("Serving mode: [%s]\n", serve_mode)
+  fmt.Printf("Verbosity level: [%d]\n", t.verbosity)
+  fmt.Printf("Serving content of: [%s]\n", serve_path)
+  fmt.Printf("Removing prefix from requested URL: [%s]\n", t.root_handler.Prefix)
+  fmt.Printf("Serving URL with prefix: [%s]\n", t.root_handler.Prefix)
+  fmt.Printf("Serving content at host: [%s]\n", t.host)
+  fmt.Printf("Trying configured port: [%d]\n", port)
+  fmt.Printf("Clean destination in copy request: [%s]\n", clean_dest)
   if t.zone_handler != nil {
     fmt.Printf("Zone header: [%s]\n", t.zone_header)
     for k := range(t.zone_handler){
@@ -371,39 +371,39 @@ func (t*app) ParseConfig() {
 
 func (t*app) Main() {
 
-	if len(os.Args) != 1 {
-		print_help()
-		return
-	}
-	fmt.Printf("%s - for help run: %s help\n", APP_TAG, os.Args[0])
+  if len(os.Args) != 1 {
+    print_help()
+    return
+  }
+  fmt.Printf("%s - for help run: %s help\n", APP_TAG, os.Args[0])
 
-	t.ParseConfig()
+  t.ParseConfig()
 
-	ln, err := t.Bind()
-	if err != nil || ln == nil {
-		t.log("ERROR", "Server stopped due to previous errors.")
-		return
-	}
+  ln, err := t.Bind()
+  if err != nil || ln == nil {
+    t.log("ERROR", "Server stopped due to previous errors.")
+    return
+  }
 
-	addr := ln.Addr().String()
-	part := strings.Split(addr, ":")
-	host, port := addr, addr
-	if part != nil && len(part) == 2 {
-		host, port = part[0], part[1]
-	}
+  addr := ln.Addr().String()
+  part := strings.Split(addr, ":")
+  host, port := addr, addr
+  if part != nil && len(part) == 2 {
+    host, port = part[0], part[1]
+  }
   t.host_string = "http://"+host+":"+port
 
-	t.log(logOpt{level: INFO}, "Starting WebDAV server at", t.host_string)
-	err = t.Run(ln)
-	if err != nil {
-		t.log(logOpt{level: ERROR}, err)
-		t.log(logOpt{level: ERROR}, "Server stopped due to previous errors.")
-	}
+  t.log(logOpt{level: INFO}, "Starting WebDAV server at", t.host_string)
+  err = t.Run(ln)
+  if err != nil {
+    t.log(logOpt{level: ERROR}, err)
+    t.log(logOpt{level: ERROR}, "Server stopped due to previous errors.")
+  }
 }
 
 func main() {
   wd := &app{}
-	wd.Main()
+  wd.Main()
 }
 
 func print_help() { fmt.Printf("%s", APP_TAG+`
